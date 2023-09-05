bac9bc3cd6b45d1229bbd52cced7c7e2df46a827
Abhinav Prakash <51137065+peda1234@users.noreply.github.com>
2023-09-05 22:55:55 +0530

    TO-DO-LIST commit
    functionality to add task, delete task , check completed task, count total task, completed task etc.

---
+++ to-do.js
@@ -0,0 +1,78 @@
+//global variable 
+let textBox=document.getElementById("text-box");
+let listItem=document.getElementById("listItem");
+let totalTaskItem=document.getElementById('totalTaskItem');
+let completedTaskItem=document.getElementById('completedTaskItem');
+
+// Add Task to list container
+function addTask()
+{
+    if(textBox.value=='')
+    {
+        alert('Enter Valid task');
+    }
+    else
+    {
+        let li=document.createElement("li");
+        li.innerHTML=textBox.value;
+        listItem.appendChild(li);
+        let span= document.createElement("span");
+        span.innerHTML='\u00d7';
+        li.appendChild(span);
+        alert('Task Added successfully');
+    }
+    textBox.value='';
+    totalTaskCount();
+    saveTask();
+}
+
+// remove task and check completed task
+listItem.addEventListener("click",function(e)
+{
+    if(e.target.tagName==='LI')
+    {
+        e.target.classList.toggle('checked');
+        completedTaskCount();
+        saveTask();
+    }
+    else if(e.target.tagName==='SPAN'){
+        e.target.parentElement.remove();
+        completedTaskCount();
+        totalTaskCount();
+        saveTask();
+        alert("Task deleted successfully");
+    }
+});
+
+// Count total number of task items in list container
+function totalTaskCount()
+{
+    let totalList=document.getElementsByTagName('li');
+    let total_task_count=totalList.length;
+    totalTaskItem.innerHTML=total_task_count;
+}
+
+//Count completed task count 
+function completedTaskCount()
+{
+    let completed_list=document.getElementsByClassName('checked');
+    let completed_task_count=completed_list.length;
+    completedTaskItem.innerHTML=completed_task_count;
+}
+
+//save task to local storage
+function saveTask()
+{
+    localStorage.setItem("data",listItem.innerHTML);
+    localStorage.setItem("countTask",totalTaskItem.innerHTML);
+    localStorage.setItem("countCompletedTask",completedTaskItem.innerHTML);
+}
+
+//get task from local storage
+function getTask()
+{
+    listItem.innerHTML=localStorage.getItem("data");
+    totalTaskItem.innerHTML=localStorage.getItem("countTask");
+    completedTaskItem.innerHTML=localStorage.getItem("countCompletedTask");
+}
+getTask();
